ff7a0ab5e4288767b311feb07a3a9821614139ee
Garret Langlois <gaslanglois@gmail.com>
2024-02-07 19:57:27 -0500

    Finished gradient descent

--- grad_desc.py
+++ grad_desc.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import subprocess
 
+
+'''
+
 flex_code = """"
 TITLE 'New Problem'     { the problem identification }
 
@@ -91,64 +94,36 @@ flexfilename = "assignment_2_heatflow.pde"
 def run_code():
     with open(flexfilename, 'w') as f:
         print(flex_code)
+    
+    subprocess.run(["C:\FkexPDE"])
+
+'''
 
 def f(r):
-    x=r[0]
-    y=r[1]
+    x = r[0]
+    y = r[1]
+    z = r[2]
     return(1./((x-3.147128)**2+(y-2.73)**2+1)+.1*x+0.01*np.cos(x*10)+0.01*np.sin(y*10))
 
-def grad(r,f0,delta=1e-3,f=f):
-    dx=[delta,0]
-    dy=[0,delta]
-    return(np.array([f(r+dx)-f0, f(r+dy)-f0])/delta)
+def grad_f(r, delta=0.01, f=f):
+    x = r[0]
+    y = r[1]
+    z = r[2]
+    grad = np.zeros(3)
+    grad[0] = (f([x+delta, y, z])-f([x-delta, y, z]))/(2*delta)
+    grad[1] = (f([x, y+delta, z])-f([x, y-delta, z]))/(2*delta)
+    grad[2] = (f([x, y, z+delta])-f([x, y, z-delta]))/(2*delta)
 
-def dotprod(v1, v2):
-    '''given two vectors, return their dot product'''
-    return v1[0]*v2[0]+v1[1]*v2[1]
+    return grad
 
-def relative_direction(v1, v2):
-    '''given two vectors, find dot(v1hat,v2hat)'''
-    return dotprod(v1,v2)/np.sqrt(dotprod(v1,v1)*dotprod(v2,v2))
+def grad_desc(r, alpha=0.01, tol=1e-6, max_iter=10000):
+    for i in range(max_iter):
+        r = r + alpha*grad_f(r)
+        if np.linalg.norm(grad_f(r)) < tol:
+            break
+    return r
 
-scale = 2
-'''Get initial grad & step'''
-r0 = np.array([0,0]) #initial guess of 0,0,0
-f0 = f(r0)
-plt.plot(r0[0], r0[1], '*')
-print(f'{r0=} {f0=}')
-current_gradient = grad(r0,f0)
+r = np.array([0, 0, 0])
+r = grad_desc(r)
+print(r)
 
-rused=[]
-rused.append([r0[0],r0[1]])
-rtried=rused.copy()
-max_cosine=.2
-for i in range(20):
-    #plt.plot(r0[0], r0[1], '*')
-    reldir=-1
-    while(reldir<max_cosine):
-        r1 = r0+scale*current_gradient
-        f1 = f(r1)
-        rtried.append([r1[0],r1[1]])
-
-        nextgrad = grad(r1,f1)
-        
-        print(f'{current_gradient=}')
-        print(f'{nextgrad=}')
-        reldir = relative_direction(current_gradient,nextgrad)
-        
-        if(reldir<max_cosine):
-            r2 = r1+scale*nextgrad #just for plotting where it would've gone
-            plt.plot([r1[0],r2[0]], [r1[1],r2[1]], 'r-')
-        
-        print(f'{i}:f({r1})={f1} -> {reldir=} {scale=}')
-        scale=scale/2
-    scale=scale*3 #undo last halving if we were OK        
-    current_gradient=nextgrad
-    r0,f0=r1,f1 #advance points
-    rused.append([r0[0],r0[1]])
-    
-rusedarray=np.array(rused)
-rtriedarray=np.array(rtried)
-
-plt.plot(rtriedarray[:,0], rtriedarray[:,1], '-*')
-plt.plot(rusedarray[:,0], rusedarray[:,1], '-*')
